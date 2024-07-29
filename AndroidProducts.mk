Rocky7842/android_device_samsung_d1x

d1x: Adapt from d1

AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_d1.mk
+    $(LOCAL_DIR)/lineage_d1x.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_d1-eng \
-    lineage_d1-user \
-    lineage_d1-userdebug
+    lineage_d1x-eng \
+    lineage_d1x-user \
+    lineage_d1x-userdebug

PixelOS-Devices/device_samsung_d1x

Change-Id: I579e4d6a32bd69115d3e7b8ca83e9dc2b62f0646

AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_d1.mk
+    $(LOCAL_DIR)/aosp_d1x.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_d1-eng \
-    aosp_d1-user \
-    aosp_d1-userdebug
+    aosp_d1x-eng \
+    aosp_d1x-user \
+    aosp_d1x-userdebug